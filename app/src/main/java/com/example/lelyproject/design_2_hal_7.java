a058fafe8a394e06d407263ab531201b2c9f06a0
AlbertusBayu <albertusbayu04@gmail.com>
2021-08-01 14:33:10 +0700

    test

---
+++ app/src/main/java/com/example/lelyproject/design_2_hal_7.java
@@ -0,0 +1,14 @@
+package com.example.lelyproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class design_2_hal_7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design2_hal7);
+    }
+}
